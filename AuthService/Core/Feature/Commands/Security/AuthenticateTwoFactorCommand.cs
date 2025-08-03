5654b085a841078d52ad9eeda6caa8b4c73461c9
Renzodv <renzodragotto50@gmail.com>
2025-08-03 19:07:13 -0300

    Se agrego propiedad enum string

--- AuthService/Core/Feature/Commands/Security/AuthenticateTwoFactorCommand.cs
+++ AuthService/Core/Feature/Commands/Security/AuthenticateTwoFactorCommand.cs
@@ -1,11 +1,13 @@
 ﻿using AuthService.Core.Enums;
 using MediatR;
+using System.Text.Json.Serialization;
 
 namespace AuthService.Core.Feature.Commands.Security
 {
     public class AuthenticateTwoFactorCommand : IRequest<bool>
     {
         public  string secretKey { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public TypeAuth typeAuth { get; set; }
 
     }
